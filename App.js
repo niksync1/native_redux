e4067908d16be3d00195eff7162e8d26a8a32bd4
niksync1 <40718182+niksync1@users.noreply.github.com>
2023-04-14 18:14:13 +0000

    one page app

--- App.js
+++ App.js
@@ -1,13 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react';
+import { Provider, useSelector, useDispatch } from 'react-redux';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+const initialState = { 
+  counter: 0,
+}
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment:(state) => {
+      state.counter += 1;
+    },
+    decrement:(state) => {
+      state.counter -=1;
+    },
+  },
+})
+const store = configureStore({
+  reducer: counterSlice.reducer,
+})
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store= {store}>
+      <Counter />
+    </Provider>
   );
+}
+
+function Counter() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  return (
+    <View>
+      <Text style={styles.text}>  
+       Counter: {counter}
+      </Text>
+      <View>
+        <Button
+          title="Increment"
+          onPress= {() => dispatch(counterSlice.actions.increment())}
+        />
+        <Button
+        title="Decrement"
+        onPress= {() => dispatch(counterSlice.actions.decrement())}
+      />      
+      </View>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -15,6 +58,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 300
   },
+  text: {
+    fontSize:32,
+    fontWeight: 'bold',
+    color: 'red',
+    marginBottom: 50
+  },
+  button:{
+    flexDirection: 'row',
+    justifyContent:'space-around',
+    width: 300,
+    height: 50
+  }
 });
